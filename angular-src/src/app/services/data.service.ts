a8469c6bb3b07fba9fcbb13aa2b64acb8ccb72cd
golf <golf_cat1@hotmail.com>
2017-06-12 23:33:35 +0700

    show list movie

--- angular-src/src/app/services/data.service.ts
+++ angular-src/src/app/services/data.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataService {
 
-  constructor() { }
+  constructor(private http:Http) { }
 
   validateRegister(user){
     if(user.firstname == undefined || user.lastname == undefined ||
@@ -24,6 +26,14 @@ export class DataService {
       return false;
     }
     else return true;
+  }
+
+
+  getAllMovie(){
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    return this.http.get('http://localhost:3000/data',{headers:headers})
+    .map(res => res.json());
   }
 
 }
