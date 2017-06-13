375e69b21e382f485debba93e75768202487eedc
golf <golf_cat1@hotmail.com>
2017-06-13 22:31:48 +0700

    movie detail

--- angular-src/src/app/component/login/login.component.ts
+++ angular-src/src/app/component/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {HomeComponent} from '../home/home.component';
+import {DataService} from '../../services/data.service';
+import {Router} from '@angular/router';
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +10,29 @@ import {HomeComponent} from '../home/home.component';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent{
+  username : String;
+  password : String;
 
-  constructor() {
+  constructor(private dataService : DataService,
+              private router : Router,
+              private flashMessage:FlashMessagesService) 
+              { }
+
+   onLoginSubmit(){
+     const user={
+       username : this.username,
+       password : this.password
+     }
+     this.dataService.authenticateUser(user).subscribe(data =>{
+        if(data.success){
+          this.dataService.storeUserData(data.token,data.user);
+           this.flashMessage.show('You are now Logged In',{cssClass:'alert-success',timeout:3000});
+          this.router.navigate(['/']);
+        }else{
+          this.flashMessage.show(data.msg,{cssClass:'alert-danger',timeout:3000});
+          this.router.navigate(['login']);
+        }
+     });
    }
+
 }
